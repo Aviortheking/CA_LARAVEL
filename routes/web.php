cae091e1eca568d2e6aaba59b40b1cfef8090250
jenoh <memint.contact@gmail.com>
2020-03-13 09:57:34 +0100

    add login page & register page

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 
 Route::get('contact-us', 'ContactUSController@contactUS');
 Route::post('contact-us', ['as' => 'contactus.store', 'uses' => 'ContactUSController@contactUSPost']);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
